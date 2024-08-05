ce169724f54af6a25df7a7a7ea98664ddabef7f5
Paul Reed <45607581+TheCoolestPaul@users.noreply.github.com>
2024-08-05 18:42:58 -0500

    edit(compat): Changed dep format

--- tankcrafting/media/lua/server/SV_Tanks.lua
+++ tankcrafting/media/lua/server/SV_Tanks.lua
@@ -1,7 +1,12 @@
 require "recipecode"
 Recipe = Recipe or {}
 Recipe.OnCreate = Recipe.OnCreate or {}
+Recipe.OnCanPerform = Recipe.OnCanPerform or {}
 
 function Recipe.OnCreate.CreateEmptyTank(items, result, player)
     result:setDelta(0)
+end
+
+function Recipe.OnCanPerform.CanMakeCompatTanks(recipe, player, item)
+    return getActivatedMods():contains("TheWorkshop(new version)")
 end
